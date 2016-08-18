9617f5d52d3e77345e622e5b743fe7fbe0782971
Sanford Student <sanfordstudent@C02QJ0RCG8WM.tld>
2016-08-18 14:09:26 -0400

    unit test fix

--- lms/djangoapps/grades/new/subsection_grade.py
+++ lms/djangoapps/grades/new/subsection_grade.py
@@ -59,7 +59,7 @@ class SubsectionGrade(object):
             BlockRecord(location, weight, score.possible)
             for location, (score, weight) in self.locations_to_weighted_scores.iteritems()
         ]
-        if course.course_version is None:
+        if getattr(course, 'course_version', None) is None:
             course.course_version = 1
 
         PersistentSubsectionGrade.save_grade(
